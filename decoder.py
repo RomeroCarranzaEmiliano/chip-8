802be4ae17febf50432f89f03e993f2d06db9c0e
Emiliano Romero Carranza <52361057+RomeroCarranzaEmiliano@users.noreply.github.com>
2021-05-15 20:29:58 -0300

    decoder naive implementation

--- decoder.py
+++ decoder.py
@@ -1,11 +1,145 @@
 """
     decoder.py
 
-
+    This serves as a decoder to read instructions from memory
 """
 
 class Decoder():
     def __init__(self):
         pass
 
+    def decode(self, opcode):
+        """
+            Receive a value in the 0x0000 format, extract the instruction code and return it
+        """
+
+        """
+            This was a naive implementation;
+            
+            A better way of doing this is grab the first and last byte of the opcode. The instruction code will be 1
+            byte or 2 bytes, the use-case of the second byte depends only on the first byte's value, so it is easy to
+            handle those cases. In the cases where the instruction code is only 1 byte, the second byte should be set
+            to 0. The result will be a different and unique two bytes number for every opcode supported, allowing to
+            access and execute the respective function in a vector or dictionary with O(1) 
+        """
+        instruction = ""
+        while True:
+            if opcode & 0xF000 == 0x0000:
+                if opcode & 0x00FF == 0x00E0:
+                    instruction = "CLS"
+                    break
+                elif opcode & 0x00FF == 0x00EE:
+                    instruction = "RET"
+                    break
+            elif opcode & 0xF000 == 0x1000:
+                instruction = "JP addr"
+                break
+            elif opcode & 0xF000 == 0x2000:
+                instruction = "CALL addr"
+                break
+            elif opcode & 0xF000 == 0x3000:
+                instruction = "SE Vx, byte"
+                break
+            elif opcode & 0xF000 == 0x4000:
+                instruction = "SNE Vx, byte"
+                break
+            elif opcode & 0xF000 == 0x5000:
+                instruction = "SE Vx, Vy"
+                break
+            elif opcode & 0xF000 == 0x6000:
+                instruction = "LD Vx, byte"
+                break
+            elif opcode & 0xF000 == 0x7000:
+                instruction = "ADD Vx, byte"
+                break
+            elif opcode & 0xF000 == 0x8000:
+                if opcode & 0x000F == 0x0000:
+                    instruction = "LD Vx, Vy"
+                    break
+                elif opcode & 0x000F == 0x0001:
+                    instruction = "OR Vx, Vy"
+                    break
+                elif opcode & 0x000F == 0x0002:
+                    instruction = "AND Vx, Vy"
+                    break
+                elif opcode & 0x000F == 0x0003:
+                    instruction = "XOR Vx, Vy"
+                    break
+                elif opcode & 0x000F == 0x0004:
+                    instruction = "ADD Vx, Vy"
+                    break
+                elif opcode & 0x000F == 0x0005:
+                    instruction = "SUB Vx, Vy"
+                    break
+                elif opcode & 0x000F == 0x0006:
+                    instruction = "SHR Vx {, Vy}"
+                    break
+                elif opcode & 0x000F == 0x0007:
+                    instruction = "SUBN Vx, Vy"
+                    break
+                elif opcode & 0x000F == 0x000E:
+                    instruction = "SHL Vx {, Vy}"
+                    break
+            elif opcode & 0xF00F == 0x9000:
+                instruction = "SNE Vx, Vy"
+                break
+            elif opcode & 0xF000 == 0xA000:
+                instruction = "LD I, addr"
+                break
+            elif opcode & 0xF000 == 0xB000:
+                instruction = "JP V0, addr"
+                break
+            elif opcode & 0xF000 == 0xC000:
+                instruction = "RND Vx, byte"
+                break
+            elif opcode & 0xF000 == 0xD000:
+                instruction = "DRW Vx, Vy, nibble"
+                break
+            elif opcode & 0xF0FF == 0xB09E:
+                instruction = "SKP Vx"
+                break
+            elif opcode & 0xF0FF == 0xE0A1:
+                instruction = "SKNP Vx"
+                break
+            elif opcode & 0xF0FF == 0xF007:
+                instruction = "LD Vx, DT"
+                break
+            elif opcode & 0xF0FF == 0xF00A:
+                instruction = "LD Vx, K"
+                break
+            elif opcode & 0xF0FF == 0xF015:
+                instruction = "LD DT, Vx"
+                break
+            elif opcode & 0xF0FF == 0xF018:
+                instruction = "LD ST, Vx"
+                break
+            elif opcode & 0xF0FF == 0xF01E:
+                instruction = "ADD I, Vx"
+                break
+            elif opcode & 0xF0FF == 0xF029:
+                instruction = "LD F, Vx"
+                break
+            elif opcode & 0xF0FF == 0xF033:
+                instruction = "LD B, Vx"
+                break
+            elif opcode & 0xF0FF == 0xF055:
+                instruction = "LD  [I], Vx"
+                break
+            elif opcode & 0xF0FF == 0xF065:
+                instruction = "LD Vx, [I]"
+                break
+            break
+
+        if instruction == "":
+            return "[ERROR] The operation", opcode, "doesn't exist"
+        else:
+            return instruction
+
+
+
+# CODE BELOW IS TESTING ONLY
+
+
+decoder = Decoder()
+print(decoder.decode(0xFE33))
 
--- opcodes.py
+++ opcodes.py
@@ -5,48 +5,41 @@
         {"opcode": opcode_function()}
 
     NOTE:
-        This implementation won't work because it doesn't allow te use of parameters in the opcodes,
-        a better solution must be found
+        This implementation(the dictionary implementation) won't work because it doesn't allow te use of parameters in
+        the opcodes, a better solution must be found
 
+    - Every instruction is 2 bytes long, so in the format 0x00
+    - Among the parameters there are:
+        - nnn or 0x0 < 0xC(12): a 12-bit value, the lowest 12 bits of the instruction
+        - n or 0x0 < 0x4(4): a 4-bit value, the lowest 4 bits of the instruction
+        - x or 0x0 < 0x4(4): a 4-bit value, lower 4 bits of the higher byte of the instruction
+        - y or 0x0 < 0x4(4): a 4-bit value, upper 4 bits of the lower byte of the instruction
+        - kk or 0x0: an 8-bit value, the lowest 8 bits of the instruction
+
+    - Possible instruction formats
+        [COP]   [COP]   [COP]   [COP]
+        [COP]   [COP]   [COP]   [ n ]
+        [COP]   [COP]   [ k ]   [ k ]
+        [COP]   [ n ]   [ n ]   [ n ]
+        [COP]   [ x ]   [   ]   [   ]
+        [COP]   [ x ]   [ y ]   [   ]
+        [COP]   [ x ]   [ k ]   [ k ]
+        [COP]   [ x ]   [ y ]   [ n ]
 """
 
 
-def _0nnn(chip):
-    """
-        This instruction is deprecated
-    """
-    pass
 
 
-def _00E0(chip):
-    """
-        Clear the display
-    """
-
-    """
-        Note: maybe this should be done with a processor's flag
-    """
-    chip.display.clear()
+instructions = [
+    [], # 0x0
+]
 
 
-def _00EE(chip):
-    """
-         Set the PC to the address at the top of the stack
-    """
-
-    # Top of the stack is 0xC given that it only has 12 positions
-    chip.registers["PC"] = 0xC
-    if chip.registers["SP"] > 0x0:
-        chip.registers["SP"] -= 0x1
-
-
-def _1nnn(chip)
-
-dictionary = {
-    "0nnn": _0nnn,
-    "00E0": _00E0,
-    "00EE": _00EE,
-    "1NNN": _1nnn,
+#dictionary = {
+    #"0nnn": _0nnn, cop=0n000
+    #"00E0": _00E0,
+    #"00EE": _00EE,
+    #"1NNN": _1nnn,
     #"2NNN": _2NNN,
     #"3XKK": _3XKK,
     #"4XKK": _4XKK,
@@ -78,4 +71,4 @@ dictionary = {
     #"FX33": _FX33,
     #"FX55": _FX55,
     #"FX65": _FX65,
-}
+#}
